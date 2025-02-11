cf8975be54528f5965ad8fbfeb6368d24d67c379
ToneMatheus <r0947882@student.thomasmore.be>
2025-02-11 16:03:59 +0100

    basic db connection

---
+++ db-terugbetaling/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace db_terugbetaling.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ db-terugbetaling/Models/Payment.cs
@@ -0,0 +1,9 @@
+public class Payment
+{
+    public int Id { get; set; }
+    public int SalesId { get; set; }
+    public DateTime Date { get; set; }
+    public string Login { get; set; }
+    public double Amount { get; set; }
+    
+}
