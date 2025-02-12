69fb89f13244cfd6a09c9d9ff5614c4272b4cede
ToneMatheus <r0947882@student.thomasmore.be>
2025-02-12 11:51:19 +0100

    Removed database code

--- db-terugbetaling/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace db_terugbetaling.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- db-terugbetaling/Models/Payment.cs
+++
@@ -1,9 +0,0 @@
-public class Payment
-{
-    public int Id { get; set; }
-    public int SalesId { get; set; }
-    public DateTime Date { get; set; }
-    public string Login { get; set; }
-    public double Amount { get; set; }
-    
-}
